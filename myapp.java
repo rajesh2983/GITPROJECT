e533b0d97c7cf2ca8b84a3aff023f29106706913
rajesh2983 <suppagarajesh@gmail.com>
2026-01-13 19:54:17 +0530

    Update studentinfo.java

--- myapp.java
+++ myapp.java
@@ -1,3 +1,26 @@
-<web-app>
-        <display-name>Hello World thankyou</display-name>
-</web-app>
+Here is the **exact text** from the image you shared:
+
+```java
+public class Student {
+
+    public static void main(String[] args) {
+
+        int rollno = 1596;
+        String sname = "KRISHNA";
+        String course = "PYTHON";
+
+        System.out.println("LEARN HERE AND LEAD ANYWHERE!!!");
+        System.out.println("My Name is: " + sname);
+        System.out.println("My Course is: " + course);
+        System.out.println("My Roll Number is: " + rollno);
+        System.out.println("==============================");
+
+        System.out.println("GOOD Evening");
+        System.out.println("WELCOME TO JENKINS");
+        System.out.println("GOOD Evening");
+        System.out.println("GOOD Evening");
+
+    }
+
+}
+```
